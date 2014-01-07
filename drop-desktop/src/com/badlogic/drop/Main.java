4e6deb61bef6e290f07383ab0652a5f17329379b
jones <jones@smartfun.com.tw>
2014-01-07 15:08:07 +0800

    init

---
+++ drop-desktop/src/com/badlogic/drop/Main.java
@@ -0,0 +1,16 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Drop";
+//		cfg.useGL20 = false;
+		cfg.width = 800;
+		cfg.height = 480;
+		
+		new LwjglApplication(new Drop(), cfg);
+	}
+}
